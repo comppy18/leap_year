835c31bdfea0ed81fffb483e9bf98391ddf51bbe
Olav Vahtras <vahtras@kth.se>
2018-10-16 11:32:35 +0200

    Parameterize tests

--- test_leap_year.py
+++ test_leap_year.py
@@ -1,10 +1,16 @@
+import pytest
 from leap_year import is_leap_year
 
-def test_leap_year():
-    assert is_leap_year(2000) == True
-    assert is_leap_year(1999) == False
-    assert is_leap_year(1998) == False
-    assert is_leap_year(1996) == True
-    assert is_leap_year(1900) == False
-    assert is_leap_year(1800) == False
-    assert is_leap_year(1600) == True
+@pytest.fixture(params=[2000, 1996, 1600])
+def true_values(request):
+    return request.param
+
+def test_true_values(true_values):
+    assert is_leap_year(true_values) == True
+
+@pytest.fixture(params=[1999, 1998, 1900, 1800])
+def false_values(request):
+    return request.param
+
+def test_false_values(false_values):
+    assert is_leap_year(false_values) == False
